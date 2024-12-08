352e5211dd2bf858bcdf8f817016b8fa25ae5be8
eloi-taniguti <eloi.taniguti@gmail.com>
2024-12-08 12:44:59 -0300

    chore: remove duplicated tests

--- tests/test_login.py
+++ tests/test_login.py
@@ -4,34 +4,39 @@ from playwright.sync_api import Page, expect
 
 from pages.login_page import LoginPage
 
+invalid_credentials = [
+    {
+        'username': 'Demouser',
+        'password': 'abc123'
+    },
+    {
+        'username': 'demouser_',
+        'password': 'xyz'
+    },
+    {
+        'username': 'demouser',
+        'password': 'nanana'
+    },
+    {
+        'username': 'demouser',
+        'password': 'abc123'
+    }
+]
+
 @pytest.fixture(scope="function", autouse=True)
 def before_each(page: Page):
     login_page = LoginPage(page)
     login_page.navigate()
     
-def test_successful_login(page: Page):
+def test_TC001_successful_login(page: Page):
     login_page = LoginPage(page)
     login_page.login_as("demouser", "abc123")
     expect(page.locator('h2')).to_have_text("Invoice List")
     expect(page).to_have_url(re.compile(r".*/account"))
 
-def test_invalid_username_login(page: Page):
+#This test will fail because last credentials are valid
+def test_TC002_invalid_credentials_login(page: Page):
     login_page = LoginPage(page)
-    login_page.login_as("Demouser", "abc123")
-    expect(page.get_by_text('Wrong username or password')).to_be_visible()
-
-def test_invalid_username_and_password_login(page: Page):
-    login_page = LoginPage(page)
-    login_page.login_as("demouser_", "xyz")
-    expect(page.get_by_text('Wrong username or password')).to_be_visible()
-
-def test_invalid_password_login(page: Page):
-    login_page = LoginPage(page)
-    login_page.login_as("demouser", "nananana")
-    expect(page.get_by_text('Wrong username or password')).to_be_visible()
-
-#This test will fail because credentials are valid
-def test_invalid_credentials_login(page: Page):
-    login_page = LoginPage(page)
-    login_page.login_as("demouser", "abc123")
+    for user in invalid_credentials:
+        login_page.login_as(user['username'], user['password'])
     expect(page.get_by_text('Wrong username or password')).to_be_visible()
