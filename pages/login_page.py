3d7e15774cd5f184a70b03af62806f5c2a3ffc5f
eloi-taniguti <eloi.taniguti@gmail.com>
2024-12-07 21:25:55 -0300

    chore: apply pom to login

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,9 +1,18 @@
-from playwright.sync_api import Page, expect
+from playwright.sync_api import Page, Locator, expect
 
 class LoginPage:
     def __init__(self, page: Page):
         self.page = page
+        self.login_title: Locator = page.locator('h1')
+        self.username_input: Locator = page.locator('input[name="username"]')
+        self.password_input: Locator = page.locator('input[name="password"]')
+        self.login_button: Locator = page.get_by_role("button", name="Login")
 
-    dev navigate(self):
-        self.goto("https://automation-sandbox-python-mpywqjbdza-uc.a.run.app/")
-        expect(page.locator('h1')).to_have_text("Login")
+    def navigate(self):
+        self.page.goto("https://automation-sandbox-python-mpywqjbdza-uc.a.run.app/")
+        expect(self.login_title).to_have_text("Login")
+
+    def login_as(self, username: str, psw: str):
+        self.username_input.fill(username)
+        self.password_input.fill(psw)
+        self.login_button.click()
--- tests/test_login.py
+++ tests/test_login.py
@@ -1,43 +1,37 @@
-import time
 import re
 import pytest
 from playwright.sync_api import Page, expect
 
+from pages.login_page import LoginPage
+
 @pytest.fixture(scope="function", autouse=True)
 def before_each(page: Page):
-        page.goto("https://automation-sandbox-python-mpywqjbdza-uc.a.run.app/")
-        expect(page.locator('h1')).to_have_text("Login")
+    login_page = LoginPage(page)
+    login_page.navigate()
     
 def test_successful_login(page: Page):
-    page.fill('input[name="username"]', "demouser")
-    page.fill('input[name="password"]', "abc123")
-    time.sleep(5)
-    page.get_by_role("button", name="Login").click()
+    login_page = LoginPage(page)
+    login_page.login_as("demouser", "abc123")
     expect(page.locator('h2')).to_have_text("Invoice List")
-    time.sleep(5)
     expect(page).to_have_url(re.compile(r".*/account"))
 
 def test_invalid_username_login(page: Page):
-    page.fill('input[name="username"]', "Demouser")
-    page.fill('input[name="password"]', "abc123")
-    page.get_by_role("button", name="Login").click()
+    login_page = LoginPage(page)
+    login_page.login_as("Demouser", "abc123")
     expect(page.get_by_text('Wrong username or password')).to_be_visible()
 
 def test_invalid_username_and_password_login(page: Page):
-    page.fill('input[name="username"]', "demouser_")
-    page.fill('input[name="password"]', "xyz")
-    page.get_by_role("button", name="Login").click()
+    login_page = LoginPage(page)
+    login_page.login_as("demouser_", "xyz")
     expect(page.get_by_text('Wrong username or password')).to_be_visible()
 
 def test_invalid_password_login(page: Page):
-    page.fill('input[name="username"]', "demouser")
-    page.fill('input[name="password"]', "nananana")
-    page.get_by_role("button", name="Login").click()
+    login_page = LoginPage(page)
+    login_page.login_as("demouser", "nananana")
     expect(page.get_by_text('Wrong username or password')).to_be_visible()
 
 #This test will fail because credentials are valid
 def test_invalid_credentials_login(page: Page):
-    page.fill('input[name="username"]', "demouser")
-    page.fill('input[name="password"]', "abc123")
-    page.get_by_role("button", name="Login").click()
+    login_page = LoginPage(page)
+    login_page.login_as("demouser", "abc123")
     expect(page.get_by_text('Wrong username or password')).to_be_visible()
